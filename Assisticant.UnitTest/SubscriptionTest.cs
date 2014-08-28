1d742615478a4d8b970b4a3eabeab3a6db2ec88e
Michael L Perry <michael@qedcode.com>
2014-08-27 22:41:20 -0500

    Test subscription and unsubscription.

---
+++ Assisticant.UnitTest/SubscriptionTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assisticant.Fields;
+using FluentAssertions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Assisticant.UnitTest
+{
+    [TestClass]
+    public class SubscriptionTest
+    {
+        private Queue<Action> _updateQueue;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _updateQueue = new Queue<Action>();
+            UpdateScheduler.Initialize(a => _updateQueue.Enqueue(a));
+        }
+
+        [TestMethod]
+        public void CanSubscribeToAComputed()
+        {
+            Observable<int> source = new Observable<int>();
+            Computed<int> target = new Computed<int>(() => source);
+
+            int count = 0;
+            target.Subscribe(i => count++);
+
+            Process();
+            count.Should().Be(1);
+            source.Value = 42;
+            Process();
+            count.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void CanUnsubscribeFromAComputed()
+        {
+            Observable<int> source = new Observable<int>();
+            Computed<int> target = new Computed<int>(() => source);
+
+            int count = 0;
+            var subscription = target.Subscribe(i => count++);
+
+            Process();
+            count.Should().Be(1);
+            source.Value = 42;
+            Process();
+            count.Should().Be(2);
+
+            subscription.Unsubscribe();
+            source.Value = 24;
+            Process();
+            count.Should().Be(2);
+        }
+
+        private void Process()
+        {
+            while (_updateQueue.Any())
+                _updateQueue.Dequeue()();
+        }
+    }
+}
