2025299e7634b3b261550fe5ab7b0299138d7232
Robert Vazan <robert.vazan@example.com>
2014-09-18 21:30:56 +0200

    Assisticant equivalent for default WPF template selector that looks up templates using wrapped object's type

---
+++ Assisticant/Markup/DefaultSelector.cs
@@ -0,0 +1,23 @@
+﻿using Assisticant.Descriptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Assisticant.Markup
+{
+    public class DefaultSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var proxy = item as PlatformProxy;
+            var element = container as FrameworkElement;
+            if (proxy != null && element != null)
+                return element.TryFindResource(new DataTemplateKey(proxy.Instance.GetType())) as DataTemplate;
+            return null;
+        }
+    }
+}
---
+++ Assisticant/Markup/DefaultSelectorExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Assisticant.Markup
+{
+    [MarkupExtensionReturnType(typeof(DefaultSelector))]
+    public class DefaultSelectorExtension : MarkupExtension
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return new DefaultSelector();
+        }
+    }
+}
