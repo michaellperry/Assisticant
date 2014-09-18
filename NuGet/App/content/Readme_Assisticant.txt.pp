f114c88f75463ca14a9b0a4be5ef52a6f575b792
Michael L Perry <michael@qedcode.com>
2014-09-18 10:00:50 -0500

    Fixed package issues. App brought in an old version of core. App namespaces included XAML. Readme instructions for WPF and Silverlight. Core depends upon System.Windows.

--- NuGet/App/content/Readme_Assisticant.txt.pp
+++ NuGet/App/content/Readme_Assisticant.txt.pp
@@ -1,8 +1,14 @@
 Add the ViewModels namespace to App.xaml:
 
+-- In Windows Store or Windows Phone applications:
 <Application
 	...
     xmlns:vm="using:$rootnamespace$.ViewModels">
+
+-- In WPF or Silverlight applications:
+<Application
+	...
+    xmlns:vm="clr-namespace:$rootnamespace$.ViewModels">
 
 
 Add the view model locator to the resource dictionary:
--- NuGet/App/content/ViewModels/MainViewModel.cs.pp
+++ NuGet/App/content/ViewModels/MainViewModel.cs.pp
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using $rootnamespace$.Models;
-using Assisticant.XAML;
+using Assisticant;
 
 namespace $rootnamespace$.ViewModels
 {
