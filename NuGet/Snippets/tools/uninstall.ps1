46e1378c07973f5188c9fec8dd67afbbdfe4a50f
Ronny Natsuo <ronny@natsuo.me>
2015-11-06 13:32:31 +0100

    Update uninstall.ps1

--- NuGet/Snippets/tools/uninstall.ps1
+++ NuGet/Snippets/tools/uninstall.ps1
@@ -15,7 +15,7 @@ param($installPath, $toolsPath, $package, $project)
 $snippetFolder = "Assisticant"
 
 # Actual script start
-$vsVersions = @("2005", "2008", "2010", "2012", "2013")
+$vsVersions = @("2005", "2008", "2010", "2012", "2013", "2015")
 
 Foreach ($vsVersion in $vsVersions)
 {
