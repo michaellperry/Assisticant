e532020579898524c3f26a977d5c5d82987cdf66
Michael L Perry <michael@qedcode.com>
2017-11-07 20:57:54 -0600

    Removed Silverlight and Windows Phone 8. Version 1.4

--- WindowsPhone/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Resources;
-
-[assembly: AssemblyTitle("Assisticant.XAML")]
-[assembly: AssemblyProduct("Assisticant.XAML")]
-[assembly: ComVisible(false)]
