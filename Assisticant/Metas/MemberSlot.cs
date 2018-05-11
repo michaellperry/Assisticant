4915f34b5dacebcfcfe81a0d4ccf3081f69a9b2f
Michael L Perry <michael.perry@improving.com>
2018-05-11 11:27:55 -0500

    Separate pass through slot.

--- Assisticant/Metas/MemberSlot.cs
+++ Assisticant/Metas/MemberSlot.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assisticant.Metas
 {
@@ -38,9 +33,27 @@ namespace Assisticant.Metas
         internal static MemberSlot Create(ViewProxy proxy, MemberMeta member)
         {
             if (member.IsCollection)
-                return new CollectionSlot(proxy, member);
+            {
+                if (member.IsObservableCollection)
+                {
+                    return new PassThroughSlot(proxy, member);
+                }
+                else
+                {
+                    return new CollectionSlot(proxy, member);
+                }
+            }
             else
-                return new AtomSlot(proxy, member);
+            {
+                if (member.IsViewModel)
+                {
+                    return new PassThroughSlot(proxy, member);
+                }
+                else
+                {
+                    return new AtomSlot(proxy, member);
+                }
+            }
         }
 
         protected void FirePropertyChanged()
---
+++ Assisticant/Metas/PassThroughSlot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Assisticant.Metas
+{
+    public class PassThroughSlot : MemberSlot
+    {
+        public PassThroughSlot(ViewProxy proxy, MemberMeta member) : base(proxy, member)
+        {
+        }
+
+        public override void SetValue(object value)
+        {
+            Member.SetValue(Instance, value);
+        }
+
+        public override object GetValue()
+        {
+            return Member.GetValue(Instance);
+        }
+
+        protected override void PublishChanges()
+        {
+        }
+
+        internal override void UpdateValue()
+        {
+        }
+    }
+}
